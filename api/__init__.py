fc63cf1c2626a02bf3e81de8b29c7462936a9bf8
Tung Dinh <tungxd301@gmail.com>
2024-07-17 14:09:23 -0700

    Add initial setup for generate ai response

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
 from .hello_api import hello_api_blueprint
 from .train_doc_api import train_doc_api_blueprint
+from .generate_ai_response_api import generate_ai_response_api_blueprint
 
 
 def register_all_apis(app):
     app.register_blueprint(hello_api_blueprint, url_prefix="/v1")
     app.register_blueprint(train_doc_api_blueprint, url_prefix="/v1")
+    app.register_blueprint(generate_ai_response_api_blueprint, url_prefix="/v1")
---
+++ api/generate_ai_response_api.py
@@ -0,0 +1,18 @@
+from flask import jsonify, Blueprint, request
+
+generate_ai_response_api_blueprint = Blueprint(
+    "generate_ai_response_api_blueprint", __name__
+)
+
+
+@generate_ai_response_api_blueprint.route("/generate-ai-response", methods=["POST"])
+def generate_ai_response():
+    body = request.get_json()
+    doc_bot_id = body["doc_bot_id"]
+    # session_id is for entity relationship which we target in the future
+    session_id = body["session_id"]
+    prompt = body["prompt"]
+    # Use doc_bot_id to query for the LLM model
+    # Your code here
+    data = {"doc_bot_id": doc_bot_id, "prompt": prompt, "session_id": session_id}
+    return jsonify(data), 200
